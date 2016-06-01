05920a06718a41e4e79387c6966328aa7abca0d2
Denis Laliberte <denis.laliberte@gmail.com>
2016-06-01 16:29:58 -0400

    cleanup source var from dotfiles alias note

--- home/note.sh
+++ home/note.sh
@@ -1,1 +1,9 @@
 echo "note : $note var : $var"
+
+export note_workspace="$HOME/Sites/notes.local/projects"
+
+## Utils
+function git_root() { basename $(git rev-parse --show-toplevel) }
+
+alias sv="source_var"
+function source_var(){ var="$note_workspace/$(git_root)/var.sh" && echo "source $var" && source $var  && source ~/note.sh source}
