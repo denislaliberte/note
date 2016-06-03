5cf43ddecee1702236a492e51835f5e91415bf30
Denis Laliberte <denis.laliberte@gmail.com>
2016-06-03 12:30:58 -0400

    add new project command

--- home/note.sh
+++ home/note.sh
@@ -5,5 +5,9 @@ export note_workspace="$HOME/Sites/notes.local/projects"
 ## Utils
 function git_root() { basename $(git rev-parse --show-toplevel) }
 
+alias np="new_project"
+function new_project(){ project_directory="$note_workspace/$(git_root)"; var=$project_directory/var.sh; mkdir -p $project_directory  && touch $var }
+
+
 alias sv="source_var"
 function source_var(){ var="$note_workspace/$(git_root)/var.sh" && echo "source $var" && source $var  && source ~/note.sh source}
